fdf6e642bde93079b7e2f410d30eca1e71e72215
L II <lii@Ls-MacBook-Pro.lan>
2022-05-28 16:53:13 -0500

    updated api url

--- src/navbar.js
+++ src/navbar.js
@@ -14,13 +14,13 @@ function NavbarApp(props) {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="justify-content-end" style={{ width: "100%" }}>
-            <Nav.Link className="nav-link" eventKey={2} href="https://game-store-backend1.herokuapp.com/games">
+            <Nav.Link className="nav-link" eventKey={2} exact path="/games">
               Games
             </Nav.Link>
-            <Nav.Link className="nav-link" eventKey={2} href="https://game-store-backend1.herokuapp.com/consoles">
+            <Nav.Link className="nav-link" eventKey={2} path="/consoles">
               Consoles
             </Nav.Link>
-            <Nav.Link className="nav-link" eventKey={2} href="https://game-store-backend1.herokuapp.com/tshirts">
+            <Nav.Link className="nav-link" eventKey={2} href="/tshirts">
               T-Shirts
             </Nav.Link>
           </Nav>
